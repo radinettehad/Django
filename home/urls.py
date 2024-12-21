d264f83168ad36289ceed034f2b06ed3df2d929b
radinettehad <radin_ettehad@yahoo.com>
2024-12-21 14:43:44 +0330

    add new index
    write new cod for added html

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from home import views
 
 urlpatterns = [
     path('home/',views.home,name='home'),
+    path('index/',views.index,name='index'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("<h1>Hello World</h1>")
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
